5b7596fc5c0644a2a6d2abaff59e24b0b458e9ae
Diego Brito <diegorbrito9@gmail.com>
2022-01-11 10:15:17 -0300

    [CHAPTER 1] fim da aula 'primeiro componente react'

---
+++ chapter-1/src/components/RepositoryList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function RepositoryList() {
+  return (
+    <section className='repository-list'>
+      <h1>Lista de repositorios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default RepositoryList;
