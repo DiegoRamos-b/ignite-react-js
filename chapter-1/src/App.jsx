311eec39be1c4176b9a9b38eb6f72c595a438902
Diego Brito <diegorbrito9@gmail.com>
2022-01-11 11:08:23 -0300

    [CHAPTER 1] fim da aula 'estado do componente'

--- chapter-1/src/App.jsx
+++ chapter-1/src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Counter from "./components/Counter";
 import RepositoryList from "./components/RepositoryList";
+
 import './styles/global.scss';
 
 function App() {
-  return <RepositoryList />;
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
 
 export default App;
