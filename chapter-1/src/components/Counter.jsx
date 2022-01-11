9a3dbfe71a0d7e6ba1f954cab5c582e51393e4e0
Diego Brito <diegorbrito9@gmail.com>
2022-01-11 11:26:17 -0300

    [CHAPTER 1] fim da aula 'fast refresh no webpack'

--- chapter-1/src/components/Counter.jsx
+++ chapter-1/src/components/Counter.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
-  
+  const [counter, setCounter] = useState(0); 
 
   return (
     <div>
--- chapter-1/webpack.config.js
+++ chapter-1/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -14,24 +15,32 @@ module.exports = {
   },
 
   devServer:{
-    static: path.resolve(__dirname, 'public')
+    static: path.resolve(__dirname, 'public'),
+    hot: true,
   },
 
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
-    })
-  ],
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })
+  ].filter(Boolean),
 
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        }
       },
       {
         test: /\.css$/,
