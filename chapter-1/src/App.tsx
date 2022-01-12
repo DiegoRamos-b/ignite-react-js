6556129d210426041546cf1ad27d6f92e43039b4
Diego Brito <diegorbrito9@gmail.com>
2022-01-12 03:35:25 -0300

    [CHAPTER 1] fim da aula 'typescript no react'

--- chapter-1/src/App.tsx
+++ chapter-1/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import RepositoryList from "./components/RepositoryList";
 
-import './styles/global.scss';
+import "./styles/global.scss";
 
 function App() {
   return (
--- chapter-1/src/services/fetchRepositories.js
+++
@@ -1,5 +0,0 @@
-export default async () => {
-  const response = await fetch("https://api.github.com/users/Drb-Diego/repos");
-  const json = await response.json();
-  return json;
-};
