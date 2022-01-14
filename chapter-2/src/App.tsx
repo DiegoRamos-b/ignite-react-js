e5c84794653e7619194601af7d7dee7b069ce9bb
Diego Brito <diegorbrito9@gmail.com>
2022-01-14 01:53:30 -0300

    [CHAPTER 2] fim da aula 'criando estilos globais'

--- chapter-2/src/App.tsx
+++ chapter-2/src/App.tsx
@@ -1,14 +1,7 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  color: red;
-  font-size: 64px;
-`;
-
 function App() {
   return (
     <div className='App'>
-      <Title>Olá mundo</Title>
+      <h1>Olá mundo</h1>
     </div>
   );
 }
