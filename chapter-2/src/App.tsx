1dc0af36bfabb8b87a63ffa8f8881a27315cf1a7
Diego Brito <diegorbrito9@gmail.com>
2022-01-14 01:33:22 -0300

    [CHAPTER 2] fim da aula 'instalando styled components'

--- chapter-2/src/App.tsx
+++ chapter-2/src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: red;
+  font-size: 64px;
+`;
+
 function App() {
   return (
     <div className='App'>
-      <h1>Ola mundo</h1>
+      <Title>Olá mundo</Title>
     </div>
   );
 }
