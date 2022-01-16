33afd5d76b1c470f746b37fe26a4051b2bd7ef65
Diego Brito <diegorbrito9@gmail.com>
2022-01-16 12:50:02 -0300

    [CHAPTER 2] fim da aula 'Criando botoes de tipo'

--- chapter-2/src/components/TransactionModal/index.tsx
+++ chapter-2/src/components/TransactionModal/index.tsx
@@ -1,6 +1,10 @@
 import Modal from 'react-modal';
-import { Container } from './style';
-import closeImg from '../../assets/close.svg'
+import { Container, TransactionTypeContainer } from './style';
+
+import closeImg from '../../assets/close.svg';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 interface TransactionModalProps {
   modalIsOpen: boolean,
@@ -24,6 +28,19 @@ export function TransactionModal ({ modalIsOpen, handleCloseModal }: Transaction
 
         <input type="text" placeholder="Titulo" />
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button>
+            <img src={ incomeImg } alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button>
+            <img src={ outcomeImg } alt="Saida" />
+            <span>Saida</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input type="text" placeholder="Categoria" />
 
         <button type="submit">
