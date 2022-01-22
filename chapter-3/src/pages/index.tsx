b5d9bb937fc03781a17d7cdf90c2dcbdb0a2966f
Diego Brito <diegorbrito9@gmail.com>
2022-01-22 18:01:17 -0300

    [CHAPTER 3] fim da aula 'configurando fonte externa'

--- chapter-3/src/pages/index.tsx
+++ chapter-3/src/pages/index.tsx
@@ -2,8 +2,8 @@ import style from "../styles/global.module.scss";
 
 export default function Home() {
   return (
-    <h1 className={style.title}>
-      Hello world
+    <h1 className={ style.title }>
+      A <span>A</span>
     </h1>
   )
 }
