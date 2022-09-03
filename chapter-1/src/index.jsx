c89381952280c1d83eeb3b8fc87d6bdeb8f51739
Diego Brito <diegorbrito9@gmail.com>
2022-09-02 21:20:09 -0300

    [ignite] [reactjs] [chapter 1] fim da aula 'configurando webpack'

--- chapter-1/src/index.jsx
+++ chapter-1/src/index.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import App from './App';
 
-function App() {
-  return (
-    <div>App</div>
-  )
+function Wrapper() {
+  return <App />;
 }
 
-export default App;
+export default Wrapper;
