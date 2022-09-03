b882398fa37405c04bf6ab0afaeb5934194ed995
Diego Brito <diegorbrito9@gmail.com>
2022-09-03 11:57:55 -0300

    [ignite] [reactjs] [chapter 1] fim da aula 'estilização da listagem'

--- chapter-1/src/App.jsx
+++ chapter-1/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Counter from './components/Counter';
 import RepositoryList from './components/RepositoryList';
 
 function App() {
   return (
     <div>
-      <Counter />
       <RepositoryList />
     </div>
   );
