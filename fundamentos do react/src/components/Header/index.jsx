6425bfaf68ae2135d8d79218409eaba72d55cb73
Diego Brito <diegorbrito9@gmail.com>
2022-09-20 18:05:44 -0300

    [ignite] [reactjs] [fundamentos do react] [estrutura da aplicação]  fim da aula 'componente header'

--- fundamentos do react/src/components/Header/index.jsx
+++ fundamentos do react/src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './header.module.css';
+import igniteLogo from '../../assets/ignite-simbol.svg';
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} alt='logo tipo do ignite' />
       <strong>iginte feed</strong>
     </header>
   );
