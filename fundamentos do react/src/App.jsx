7ae0afa8b6918f5872fa9ade2b34c81cc169b6fd
Diego Brito <diegorbrito9@gmail.com>
2022-09-19 22:19:08 -0300

    [ignite] [reactjs] [fundamentos do react] [iniciando com react]  fim da aula 'componentes'

--- fundamentos do react/src/App.jsx
+++ fundamentos do react/src/App.jsx
@@ -1,5 +1,17 @@
+import { Post } from './Post';
+
 function App() {
-  return <h1>Hello World !</h1>;
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
 }
 
 export default App;
