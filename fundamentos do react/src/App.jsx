ac9800571ca828b608608718b57394ce41c9de9d
Diego Brito <diegorbrito9@gmail.com>
2022-09-19 22:26:15 -0300

    [ignite] [reactjs] [fundamentos do react] [iniciando com react]  fim da aula 'propriedades'

--- fundamentos do react/src/App.jsx
+++ fundamentos do react/src/App.jsx
@@ -3,13 +3,14 @@ import { Post } from './Post';
 function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author='Diego Ramos'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptates non totam esse, sint nesciunt quos quo modi consequuntur at voluptatibus beatae, nulla aspernatur vitae explicabo quae! Commodi, porro fugiat.'
+      />
+      <Post
+        author='Tatiana Ramos'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptates non totam esse, sint nesciunt quos quo modi consequuntur at voluptatibus beatae, nulla aspernatur vitae explicabo quae! Commodi, porro fugiat.'
+      />
     </div>
   );
 }
--- fundamentos do react/src/Post.jsx
+++ fundamentos do react/src/Post.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export function Post() {
-  return <p>Post</p>;
+export function Post({ author, content }) {
+  return (
+    <>
+      <h1>{author}</h1>
+      <p>{content}</p>
+    </>
+  );
 }
