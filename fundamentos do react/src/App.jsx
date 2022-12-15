f4ea9cfeaecae82518dcaddc393bb3cb3150bf29
Diego Ramos <diegorbrito9@gmail.com>
2022-12-15 11:40:18 -0300

    [ignite] [reactjs] [fundamentos do react] [Estrutura da aplicação]  fim da aula 'css global'

--- fundamentos do react/src/App.jsx
+++ fundamentos do react/src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 
 export function App() {
   return (
-    <div>
-      <Post
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem nemo at vero quo, nobis sed cum"
-        author="Diego Ramos"
-      />
-      <Post
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem nemo at vero quo, nobis sed cum"
-        author="Tatiana Ramos"
-      />
-    </div>
+    <>
+      <Header />
+
+      <div>
+        <Post
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem nemo at vero quo, nobis sed cum"
+          author="Diego Ramos"
+        />
+        <Post
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem nemo at vero quo, nobis sed cum"
+          author="Tatiana Ramos"
+        />
+      </div>
+    </>
   );
 }
