64a6f842a1cc292ee7c0b4421db5bae4a51473fb
Diego Ramos <diegorbrito9@gmail.com>
2022-12-31 16:29:26 -0300

    [ignite] [reactjs] [fundamentos do react] [estrutura da aplicação]  fim da aula 'componente: sidebar'

--- fundamentos-reactjs/estrutura-da-aplicacao/src/App.jsx
+++ fundamentos-reactjs/estrutura-da-aplicacao/src/App.jsx
@@ -1,19 +1,28 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
 function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Diego Ramos"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eum cumque suscipit numquam. Commodi, molestias odit consequuntur ipsam quia dicta recusandae maxime velit ducimus suscipit dolorum facere, nam sunt veniam!"
-      />
 
-      <Post
-        author="Tatiana Ramos"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eum cumque suscipit numquam. Commodi, molestias odit consequuntur ipsam quia dicta recusandae maxime velit ducimus suscipit dolorum facere, nam sunt veniam!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Ramos"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eum cumque suscipit numquam. Commodi, molestias odit consequuntur ipsam quia dicta recusandae maxime velit ducimus suscipit dolorum facere, nam sunt veniam!"
+          />
+
+          <Post
+            author="Tatiana Ramos"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eum cumque suscipit numquam. Commodi, molestias odit consequuntur ipsam quia dicta recusandae maxime velit ducimus suscipit dolorum facere, nam sunt veniam!"
+          />
+        </main>
+      </div>
     </>
   );
 }
