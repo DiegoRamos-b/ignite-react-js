2dc7ff79d4b0cf0f9adbfb0099710ffd966cafbb
Diego Ramos <diegorbrito9@gmail.com>
2022-12-31 16:48:10 -0300

    [ignite] [reactjs] [fundamentos do react] [estrutura da aplicação]  fim da aula 'Finalizando Sidebar'

--- fundamentos-reactjs/estrutura-da-aplicacao/src/components/Sidebar.jsx
+++ fundamentos-reactjs/estrutura-da-aplicacao/src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { PencilLine } from "phosphor-react";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -11,12 +13,16 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/Drb-Diego.png" />
         <strong>Diego Ramos</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
