8838b0cc96e92e27336649f4f36627f0b0ba2370
Diego Ramos <diegorbrito9@gmail.com>
2023-01-12 11:07:59 -0300

    [ignite] [reactjs] [criando SPAs com reactJs] [Paginas e rotas]  fim da aula 'Pagina: History'

--- criando-spas-com-reactjs/paginas-e-rotas/src/pages/History/index.tsx
+++ criando-spas-com-reactjs/paginas-e-rotas/src/pages/History/index.tsx
@@ -1,3 +1,45 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-  return <div>History</div>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <td>Concluido</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <td>Concluido</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
