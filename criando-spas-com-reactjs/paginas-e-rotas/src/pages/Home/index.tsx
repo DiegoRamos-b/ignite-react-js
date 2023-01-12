096efa69b0548aa3c6e61caf66adb15ab8d0f4b9
Diego Ramos <diegorbrito9@gmail.com>
2023-01-12 10:52:04 -0300

    [ignite] [reactjs] [criando SPAs com reactJs] [Paginas e rotas]  fim da aula 'Aprimorando os inputs'

--- criando-spas-com-reactjs/paginas-e-rotas/src/pages/Home/index.tsx
+++ criando-spas-com-reactjs/paginas-e-rotas/src/pages/Home/index.tsx
@@ -17,12 +17,25 @@ export function Home() {
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
-            type="text"
             placeholder="De um nome para o seu projeto"
+            list="task-suggestions"
           />
 
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
           <label htmlFor="minutesAmout">Durante</label>
-          <MinutesAmoutInput id="minutesAmout" type="number" placeholder="00" />
+          <MinutesAmoutInput
+            id="minutesAmout"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>Minutos.</span>
         </FormContainer>
