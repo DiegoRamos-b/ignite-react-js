0b318aa0c534695d4164a3feb82a532e35ae66ba
Diego Ramos <diegorbrito9@gmail.com>
2023-01-14 20:31:11 -0300

    [ignite] [reactjs] [criando SPAs com reactJs] [Funcionalidades da aplicação]  fim da aula 'Iniciando novo ciclo'

---
+++ criando-spas-com-reactjs/funcionalidades-da-aplicacao/src/pages/Home/index.tsx
@@ -0,0 +1,112 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import { Play } from 'phosphor-react'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmoutInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+import { useState } from 'react'
+
+const validationNewCycleSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmout: zod.number().min(5).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof validationNewCycleSchema>
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmout: number
+}
+
+export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(validationNewCycleSchema),
+    defaultValues: {
+      task: '',
+      minutesAmout: 0,
+    },
+  })
+
+  const handleCreateNewCycle = ({ task, minutesAmout }: NewCycleFormData) => {
+    const id = new Date().getTime().toString()
+
+    const newCycle = {
+      id,
+      task,
+      minutesAmout,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    reset()
+  }
+
+  const activeCycle = cycles.find(
+    (currentCycle) => currentCycle.id === activeCycleId,
+  )
+
+  console.log(activeCycle)
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="De um nome para o seu projeto"
+            {...register('task')}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmout">Durante</label>
+          <MinutesAmoutInput
+            id="minutesAmout"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmout', { valueAsNumber: true })}
+          />
+
+          <span>Minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+          <Play />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
