fa1958616f0d8c56d9a9a6a2167d6e7be539291a
Diego Ramos <diegorbrito9@gmail.com>
2023-01-17 11:53:26 -0300

    [ignite] [reactjs] [criando SPAs com reactJs] [Contexto no React]  fim da aula 'contexto entre rotas'

--- criando-spas-com-reactjs/context-no-react/src/App.tsx
+++ criando-spas-com-reactjs/context-no-react/src/App.tsx
@@ -4,11 +4,19 @@ import { GlobalStyle } from './styles/global'
 
 import { defaultTheme } from './styles/themes/default'
 
+import { CyclesContextProvider } from './context/CyclesContext'
+import { BrowserRouter } from 'react-router-dom'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Router />
+
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
