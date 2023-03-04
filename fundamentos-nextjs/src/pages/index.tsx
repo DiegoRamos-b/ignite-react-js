507d1146c9a4810ddded0e0fc01aa5d33cf74b81
Diego Ramos <diegorbrito9@gmail.com>
2023-03-04 11:44:47 -0300

    [ignite] [reactjs] [fundamentos next.Js] [Estrutura visual]  fim da aula 'Configurando stitches'

--- fundamentos-nextjs/src/pages/index.tsx
+++ fundamentos-nextjs/src/pages/index.tsx
@@ -1,3 +1,15 @@
+import { styled } from "@/styles";
+
+const Button = styled("button", {
+  background: "$test",
+  fontSize: 60,
+});
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Hello world !</h1>
+      <Button>Enviar</Button>
+    </>
+  );
 }
