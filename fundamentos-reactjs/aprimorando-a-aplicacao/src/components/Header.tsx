bc9e8b0c9cea3accb0397028712a423f79d232d8
Diego Ramos <diegorbrito9@gmail.com>
2023-04-21 13:23:44 -0300

    [ignite] [reactjs] [fundamentos next.Js] recomeçando o modulo

--- fundamentos-reactjs/aprimorando-a-aplicacao/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Header.module.css";
-
-export function Header() {
-  return (
-    <header className={styles.header}>
-      <img src="/ignite-logo.svg" />
-    </header>
-  );
-}
