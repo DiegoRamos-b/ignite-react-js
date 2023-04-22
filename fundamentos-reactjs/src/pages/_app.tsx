5d3b0e3f6ff2989f17f4eb391d448c3a4ecdf55a
Diego Ramos <diegorbrito9@gmail.com>
2023-04-22 11:14:35 -0300

    [ignite] [reactjs] [fundamentos next.Js] [estrutura visual] fim da aula 'cabeçalhos da aplicação'

--- fundamentos-reactjs/src/pages/_app.tsx
+++ fundamentos-reactjs/src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import type { AppProps } from "next/app";
 
+import Image from "next/image";
+
 import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img
+          src="https://raw.githubusercontent.com/rocketseat-education/04-ignite-shop/a06358d9d761af6b54ca269e722d2015b3f137b5/src/assets/logo.svg"
+          alt="logo"
+        />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
